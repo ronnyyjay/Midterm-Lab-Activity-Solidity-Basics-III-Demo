cbfcb88a87a1a6ebc1579644d9eaec0236c46d42
AaronRamales <99706675+ronnyyjay@users.noreply.github.com>
2024-03-03 20:28:01 +0800

    Create 3_Structs_demo.sol

---
+++ 3_Structs_demo.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract StructsDemo {
+    struct Car {
+        string model;
+        uint year;
+        address owner;
+    }
+
+    Car public car;
+    Car[] public cars;
+
+    mapping(address => Car[]) public carsByOwner;
+
+    function someApplications() external {
+        Car memory toyota = Car("Toyota", 1990, msg.sender);
+        Car memory lambo = Car({year: 1990, model: "Lamborghini", owner: msg.sender});
+        Car memory tesla;
+
+        tesla.model = "Tesla";
+        tesla.year = 1983;
+        tesla.owner = msg.sender;
+
+        cars.push(toyota);
+        cars.push(lambo);
+        cars.push(tesla);
+
+        cars.push(Car("Ferrari", 2020, msg.sender));
+
+        Car storage _car = cars[0];
+        _car.year = 1990;
+        delete _car.owner;
+
+        delete cars[1];
+    }
+}
