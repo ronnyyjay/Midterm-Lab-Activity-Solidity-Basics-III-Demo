adfe5a0488e5d13d9c348968e5b49c0034c27a7d
AaronRamales <99706675+ronnyyjay@users.noreply.github.com>
2024-03-03 20:33:55 +0800

    Update 1_destructuring.sol

--- 1_destructuring.sol
+++ 1_destructuring.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 contract DestructuringSol {
     function multiOutput() public pure returns(uint, bool) {
@@ -24,5 +24,5 @@ contract DestructuringSol {
     }
 
     // Define an event to log the values
-    event LogValues(uint a, bool b);
+    event LogValues(uint indexed a, bool indexed b);
 }
